cab2c6390357e55e2b4a849ccc0fbd3f58c73c39
Alexei Zaviruha <a.zaviruha@gmail.com>
2015-06-12 18:10:32 +0300

    Evil + Tern + auto-complete = friendship!

---
+++ .emacs.d/themes.el
@@ -0,0 +1,31 @@
+
+; ================================================================== ;
+; =============================== Themes =========================== ;
+; ================================================================== ;
+;(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+;(add-to-list 'load-path "~/.emacs.d/themes")
+;; Uncomment this to increase font size
+;; (set-face-attribute 'default nil :height 140)
+
+;; ========== Solarized-dark theme ===========;
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector [default bold shadow italic underline bold bold-italic bold])
+ '(ansi-color-names-vector (vector "#eaeaea" "#d54e53" "#b9ca4a" "#e7c547" "#7aa6da" "#c397d8" "#70c0b1" "#000000"))
+ '(custom-enabled-themes (quote (sanityinc-solarized-dark)))
+ '(custom-safe-themes (quote ("4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" default)))
+ '(fci-rule-color "#2a2a2a")
+ '(haskell-process-auto-import-loaded-modules t)
+ '(haskell-process-log t)
+ '(haskell-process-suggest-remove-import-lines t)
+ '(tab-stop-list (quote (4 8 16 24 32 40 48 56 64 72 80 88 96 104 112 120)))
+ '(tab-width 4))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- .emacs.d/user.el
+++ .emacs.d/user.el
@@ -1,15 +1,18 @@
 ;; Custom Emacs
 
+;; Preparation
+;; cd ~/.emacs.d/vendor/tern/
+;; sudo npm i
+;; sudo ln -s -T /home/azaviruha/.emacs.d/vendor/tern/bin/tern /usr/bin/tern
+
+(load "~/.emacs.d/themes.el")
+
 ;; Place downloaded elisp files in this directory. You'll then be able
 ;; to load them.
 (add-to-list 'load-path "~/.emacs.d/vendor")
-
-;; Themes
-;(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-;(add-to-list 'load-path "~/.emacs.d/themes")
-;; Uncomment this to increase font size
-;; (set-face-attribute 'default nil :height 140)
-;(load-theme 'tomorrow-night-bright t)
+(add-to-list 'load-path "~/.emacs.d/vendor/tern/")
+(add-to-list 'load-path "~/.emacs.d/vendor/tern/emacs/")
+(autoload 'tern-mode "tern.el" nil t)
 
 
 ; ================================================================== ;
@@ -33,3 +36,59 @@
 
 (require 'evil)
 (evil-mode 1)
+
+
+; ================================================================== ;
+; =========================== Generic IDE ========================== ;
+; ================================================================== ;
+;; Auto-complete mode
+;; should be loaded after yasnippet so that they can work together
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+(ac-config-default)
+(global-auto-complete-mode t)
+;; ;; Start auto-completion after 2 characters of a word
+(setq ac-auto-start 2)
+;; ;; case sensitivity is important when finding matches
+(setq ac-ignore-case nil)
+
+
+; ================================================================== ;
+; =========================== JavaScript =========================== ;
+; ================================================================== ;
+(add-hook 'js-mode-hook  'js2-minor-mode)
+;; (add-hook 'js2-mode-hook 'ac-js2-mode)
+(setq js2-highlight-level 3)
+(add-hook 'js-mode-hook (lambda () (tern-mode t)))
+(eval-after-load 'tern-mode
+   '(progn
+      (require 'tern-auto-complete)
+      (tern-ac-setup)))
+
+
+(evil-define-key 'insert tern-mode-keymap 
+  (kbd "M-.")   'tern-find-definition 
+  (kbd "C-SPC") (lambda () 
+				 (interactive)
+				 (tern-ac-setup)
+				 (tern-ac-complete)))
+
+(evil-define-key 'normal tern-mode-keymap 
+  (kbd "M-.")   'tern-find-definition 
+  (kbd "C-SPC") (lambda () 
+				 (interactive)
+				 (tern-ac-setup)
+				 (tern-ac-complete)))
+
+(evil-define-key 'motion tern-mode-keymap 
+  (kbd "M-.")   'tern-find-definition 
+  (kbd "C-SPC") (lambda () 
+				 (interactive)
+				 (tern-ac-setup)
+				 (tern-ac-complete)))
+
+
+
+;; (define-key evil-insert-state-map (kbd "M-.") 'tern-find-definition)
+;; (define-key evil-motion-state-map (kbd "M-.") 'tern-find-definition)
+;; (define-key evil-normal-state-map (kbd "M-.") 'tern-find-definition)
